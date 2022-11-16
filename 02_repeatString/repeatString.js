bab869d53711732027d2c48ff22280e9c0c184c4
Michael Chan <chan.myko@gmail.com>
2022-11-16 18:07:31 -0500

    02_repeatSting

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(repeatString, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    let repeatedString = "";
+    for (let i = 0; i < num; i++) {
+        repeatedString += repeatString;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
