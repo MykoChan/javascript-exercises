0a1d62473617d2a43676127b8a57e67747de75ae
Michael Chan <chan.myko@gmail.com>
2022-11-17 00:53:15 -0500

    08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	// result = 0;
+  // for (let x of array) {
+  //   result += x;
+  // }
+  // return result;
+  return array.reduce((total, current) => total + current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.length ? array.reduce((total, current) => total * current, 1) : 0;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  product = 1;
+  for (let i = num; i > 1; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
