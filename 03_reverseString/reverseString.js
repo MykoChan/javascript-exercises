70498d68eb113b16c66e27a317fd7aac8a429e02
Michael Chan <chan.myko@gmail.com>
2022-11-16 22:22:07 -0500

    03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+// const reverseString = function(s) {
+//     reversedString = ""
+//     for (let x of s) {
+//         reversedString = x + reversedString;
+//     }
+//     return reversedString;
+// };
+const reverseString = function(string) {
+    return string.split('').reverse().join('');
 };
 
 // Do not edit below this line
