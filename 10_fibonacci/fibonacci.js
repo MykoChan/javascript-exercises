5aef6d7ecf8a0fe2665c11f246f9193a133e1cc0
Michael Chan <chan.myko@gmail.com>
2022-12-04 22:41:46 -0500

    10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    }
+    if (num == 0) return 0;
+    f = [1, 1]
+    for (let i = 2; i < num; i++) {
+        f.push(f[i-2]+f[i-1]);
+    }
+    return f[num-1];
 };
 
 // Do not edit below this line
