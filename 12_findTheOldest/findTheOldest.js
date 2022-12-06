07fb268d7101f2d00f79e16efc935b19bc9a87fe
Michael Chan <chan.myko@gmail.com>
2022-12-05 23:14:58 -0500

    12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(a) {
+    return a.reduce((oldest, person) => {
+        return getAge(oldest) < getAge(person) ? person : oldest; 
+    });
 };
+
+function getAge(person) {
+    return person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth : new Date().getFullYear() - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
